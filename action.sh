b0c2ec54b77c45b5267c6fa0221d23ab77a26c38
TheKingDave <david@langheiter.com>
2025-02-11 03:02:50 +0100

    Take GITHUB_SERVER_URL into consideration

--- action.sh
+++ action.sh
@@ -1,4 +1,10 @@
 #!/bin/sh
 
 set -eux
-git clone --progress --single-branch --branch "${INPUTS_BRANCH:-${GITHUB_REF##*/}}" "https://${GITHUB_REPOSITORY_OWNER}:${GITHUB_TOKEN}@github.com/${GITHUB_REPOSITORY}.git" .
+
+SERVER_URL=${GITHUB_SERVER_URL}
+if [[ "$SERVER_URL" =~ ^https?://(.+/?)$ ]]; then
+  SERVER_URL="${BASH_REMATCH[1]}"
+fi
+
+git clone --progress --single-branch --branch "${INPUTS_BRANCH:-${GITHUB_REF##*/}}" "https://${GITHUB_REPOSITORY_OWNER}:${GITHUB_TOKEN}@${SERVER_URL}/${GITHUB_REPOSITORY}.git" .
